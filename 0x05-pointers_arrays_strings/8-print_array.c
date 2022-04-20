860f72577f16e7264950e2a25c6bb7de28f2288a
praisino <praisetaiwo@gmail.com>
2022-04-20 14:41:22 -0700

    file 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -20,9 +20,5 @@ void print_array(int *a, int n)
 			printf(", ");
 		}
 	}
-	// for (i = 0; i < (n - 1); i++)
-	// 	printf("%d, ", a[i]);
-	// 	if (i == (n - 1))
-	// 		printf("%d", a[n - 1]);
 	printf("\n");
 }
