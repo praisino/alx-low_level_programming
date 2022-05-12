952b42f87d607ebb8acf7d814250bda8b4c03b03
praisino <praisetaiwo@gmail.com>
2022-05-11 18:00:18 -0700

    renamed preprocessor folder

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,7 +10,6 @@
 int main(void)
 {
 	int num;
-
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
@@ -20,8 +19,6 @@ int main(void)
 		putchar(',');
 		putchar(' ');
 	}
-
 	putchar('\n');
-
 	return (0);
 }
