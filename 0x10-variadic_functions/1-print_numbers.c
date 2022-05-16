b4adf5d47f2db7273b74c0bba224d2dd007026ba
praisino <praisetaiwo@gmail.com>
2022-05-16 15:31:41 -0700

    fixed 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,27 +1,30 @@
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include "variadic_functions.h"
 /**
- * sum_them_all - Entry Point
- * @n: variadic inputs
- * Return: sum
+ * print_numbers - Entry Point
+ * @separator: comma space
+ * @n: elements to be printed
+ * Return: void
  */
-int sum_them_all(const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
-	unsigned int i, sum = 0;
+	unsigned int i;
 
-	if (n == 0)
-		return (0);
+	if (separator == NULL)
+		return;
 
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(valist, const unsigned int);
+		printf("%d", va_arg(valist, const unsigned int));
+
+		if (i != (n - 1))
+			printf("%s", separator);
 	}
+	printf("\n");
 
 	va_end(valist);
-
-	return (sum);
 }
